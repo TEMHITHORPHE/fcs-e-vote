0725aa5f6647e04875f4c2f4ea14ab1fa9ff2f91
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-01-29 05:22:53 +0100

    Fix error relay

--- routes/index-route.js
+++ routes/index-route.js
@@ -6,21 +6,29 @@ const { supabase } = require('../model/db');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   console.log("REQ-[GET]-[/] : ", req.headers['user-agent']);
-  res.render('index', { title: 'Home Page' });
+  return res.render('index', { title: 'Home Page', page_alerts: "null" });
 });
 
 
 // @route '/'
 router.post('/', async function (req, res, next) {
   console.log("REQ-[POST]-[/] : ", req.body);
-  if (req.session.isNew) {
+  if (req.body && req.body.codebox) {
 
     let { data: AccessCodes, error } = await supabase
       .from('AccessCodes')
       .select('is_used')
-      console.log('AccessCodes: ', AccessCodes);
-      // if (AccessCodes)
-    // req.session.access_code = 'qwertyuiop';
+      .eq('tag', process.env.E_VOTE_TAG)
+      .eq('access_code', req.body.codebox).single();
+
+    console.log('access_code: ', AccessCodes);
+
+    if (AccessCodes || AccessCodes.is_used) {
+      return res.render('index', {
+        title: 'Home Page', page_alerts: [{ type: 'error', message: 'Error!... Access Code Invalid' }]
+      });
+    }
+
   }
-  res.render('index', { title: 'Home Page' });
+  return res.render('index', { title: 'Home Page'});
 });
