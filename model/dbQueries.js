391368701dd6a109ea214c4c4f67c516b2eea1e9
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-02-02 23:33:45 +0100

    cache and live data query

--- model/dbQueries.js
+++ model/dbQueries.js
@@ -20,27 +20,86 @@ class Nominator {
 	}
 
 	batchAddNominators(nominators) {
+		console.log("Batch Adding ... ");
 		if (nominators.length > 0) {
-			this.nominators = new Set(nominators);
+			for (let index = 0; index < nominators.length; index++) {
+				// const nominator = nominators[index];
+				this.addNominator(nominators[index].access_code);
+			}
+			// this.nominators = new Set(nominators);
 			this.bootstrapped = true;
+			console.log("[dbQueries.js]-[Nominators-Added]: ", nominators);
 			return true;
 		}
 		return false;
 	}
 
-
-
 }
 
-
 const nominators = new Nominator();
-
 
 module.exports.nominators = nominators;
 module.exports.getNominators = async function getNominators() {
 	let { data: AccessCodes, error } = await supabase
 		.from('AccessCodes')
 		.select('access_code')
+		.eq('is_used', true);
+	console.log("[dbQueries.js]-[getNominators]: ", AccessCodes);
 	if (AccessCodes === null || error) return [];
 	return AccessCodes;
 };
+
+
+
+
+// const unit_set = new Set();
+// for (let index = 0; index < member_data.length; index++) {
+// 	const member = member_data[index];
+// 	const unit = member.unit;
+// 	if (unit_set.has(unit)) {
+// 		members_data_by_units[unit].push(member);
+// 	}
+// 	else {
+// 		unit_set.add(unit);
+// 		members_data_by_units[unit] = [];
+// 		members_data_by_units[unit].push(member);
+// 	}
+// }
+
+
+
+
+module.exports.members = async function member_data() {
+
+	const { data, error } = await
+		supabase
+			.from('Members')
+			.select('first_name, last_name, gender, level, unit, campus')
+			.neq('level', 100)
+			.neq('level', 500);
+
+	if (error || data === null) return [];
+
+	// data.map(function (member, index, member_data) {
+	// 	member_data[index].unit = member.unit.toLowerCase().split(" ").join("_");
+	// });
+
+	const unit_set = new Set();
+	for (let index = 0; index < data.length; index++) {
+		const member = data[index];
+		const unit = member.unit;
+		if (unit_set.has(unit)) {
+			members_data_by_units[unit].push(member);
+		}
+		else {
+			unit_set.add(unit);
+			members_data_by_units[unit] = [];
+			members_data_by_units[unit].push(member);
+		}
+	}
+
+	// console.log("Members: ", data);
+}
+
+const members_data_by_units = {};
+module.exports.members_data_by_units = members_data_by_units;
