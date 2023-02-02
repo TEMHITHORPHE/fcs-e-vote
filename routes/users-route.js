069313d15387c0ae982d6eeeeaba8a0e374a4b06
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-02-02 23:38:11 +0100

    added mock random string generator

--- routes/users-route.js
+++ routes/users-route.js
@@ -2,7 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/admin/random', function (req, res, next) {
+
+  // program to generate random strings
+
+  // declare all characters
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  }
+
+  for (let index = 0; index < 500; index++) {
+    const element = array[index];
+  }
+
+  console.log(generateString(5));
   res.send('respond with a resource');
 });
 
